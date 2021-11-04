7e8f7e2e05b1acc006432fd07d31115fd58763e2
dr-redtec <jan.leonardi@stud.fra-uas.de>
2021-11-04 16:09:03 +0100

    Added Code Video and Audio

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 const container = document.querySelector(".container");
-const coffees = [
-  {
+const coffees = [{
     name: "Perspiciatis",
     image: "images/coffee1.jpg"
   },
@@ -37,28 +36,74 @@ const coffees = [
     image: "images/coffee9.jpg"
   }
 ];
-// const showCoffees = () => {
-//   let output = "";
-//   coffees.forEach(
-//     ({ name, image }) =>
-//       (output += `
-//               <div class="card">
-//                 <img class="card--avatar" src=${image} />
-//                 <h1 class="card--title">${name}</h1>
-//                 <a class="card--link" href="#">Taste</a>
-//               </div>
-//               `)
-//   );
-//   container.innerHTML = output;
-// };
-//
-// document.addEventListener("DOMContentLoaded", showCoffees);
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
-  });
+function getUserMedia(constraints) {
+  // if Promise-based API is available, use it
+  if (navigator.mediaDevices) {
+    return navigator.mediaDevices.getUserMedia(constraints);
+  }
+
+  // otherwise try falling back to old, possibly prefixed API...
+  var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+  if (legacyApi) {
+    // ...and promisify it
+    return new Promise(function(resolve, reject) {
+      legacyApi.bind(navigator)(constraints, resolve, reject);
+    });
+  }
 }
+
+function getStream(type) {
+  if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+    alert('User Media API not supported.');
+    return;
+  }
+
+  var constraints = {};
+  constraints[type] = true;
+
+  getUserMedia(constraints)
+    .then(function(stream) {
+      var mediaControl = document.querySelector(type);
+
+      if ('srcObject' in mediaControl) {
+        mediaControl.srcObject = stream;
+      } else if (navigator.mozGetUserMedia) {
+        mediaControl.mozSrcObject = stream;
+      } else {
+        mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+      }
+
+      mediaControl.play();
+    })
+    .catch(function(err) {
+      alert('Error: ' + err);
+    });
+  // const showCoffees = () => {
+  //   let output = "";
+  //   coffees.forEach(
+  //     ({ name, image }) =>
+  //       (output += `
+  //               <div class="card">
+  //                 <img class="card--avatar" src=${image} />
+  //                 <h1 class="card--title">${name}</h1>
+  //                 <a class="card--link" href="#">Taste</a>
+  //               </div>
+  //               `)
+  //   );
+  //   container.innerHTML = output;
+  // };
+  //
+  // document.addEventListener("DOMContentLoaded", showCoffees);
+
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err));
+    });
+  }
