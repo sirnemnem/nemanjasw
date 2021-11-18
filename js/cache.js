c4117ef6a9ef41e1dde1aa5bcbe7ec967d9002ce
dr-redtec <jan.leonardi@stud.fra-uas.de>
2021-11-18 16:39:05 +0100

    cache001

---
+++ js/cache.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    const asText = document.getElementById('asText');
+    const textbox = document.getElementById('textbox');
+
+    asText.addEventListener('change', function(e) {
+        const file = asText.files[0];
+        const textType = /text.*/;
+
+        if (file.type.match(textType)) {
+            const reader = new FileReader();
+
+            reader.onload = function(e) {
+                textbox.innerText = reader.result;
+            }
+
+            reader.readAsText(file);
+        } else {
+            textbox.innerText = "Dateityp nicht unterstützt";
+        }
+    });
+}
