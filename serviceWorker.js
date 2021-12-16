be2d349930b7222c6f1c0dff2b770ae426444719
dr-redtec <jan.leonardi@stud.fra-uas.de>
2021-12-16 13:26:30 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -21,3 +21,17 @@ self.addEventListener("fetch", fetchEvent => {
     })
   )
 })
+
+self.addEventListener('activate', event => {
+
+  // Delete all Service Worker Caches
+  caches.keys().then(cacheNames => {for (let name of cacheNames) {caches.delete(name);}});
+
+  // Unregister all Service Workers
+  self.registration.unregister()
+
+    .then(() => self.clients.matchAll())
+
+    .then((clients) => clients.forEach(client => client.navigate(client.url)))
+
+});
