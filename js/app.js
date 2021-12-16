bf3156930235955057c45efbc3773ee49e0cc92c
dr-redtec <jan.leonardi@stud.fra-uas.de>
2021-12-16 12:58:20 +0100

    asd

--- js/app.js
+++ js/app.js
@@ -52,7 +52,7 @@ const coffees = [
 //   container.innerHTML = output;
 // };
 
-document.addEventListener("DOMContentLoaded", showCoffees);
+//document.addEventListener("DOMContentLoaded", showCoffees);
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
