fa05c3c13e5b9ac8b30ee757791b74751b072f3e
dr-redtec <jan.leonardi@stud.fra-uas.de>
2021-12-16 14:19:59 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const staticDevCoffee = "v2.11"
+const staticDevCoffee = "v2.12"
 const assets = [
   "/",
   "/index.html",
@@ -33,7 +33,7 @@ self.addEventListener('fetch', function(event) {
 
 self.addEventListener('activate', function(event) {
 
-  var cacheAllowlist = [];
+  var cacheAllowlist = ['v2.12'];
 
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
